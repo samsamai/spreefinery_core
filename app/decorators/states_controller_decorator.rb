0a111883c5cda18bc6d110e602fe9d35d554a428
Zee Yang <zee.yang@gmail.com>
2013-03-12 23:30:51 -0700

    Update state controller to return json

--- app/decorators/states_controller_decorator.rb
+++ app/decorators/states_controller_decorator.rb
@@ -2,14 +2,7 @@ Spree::StatesController.class_eval do
 
   def by_country
     states = Spree::State.where(:country_id => params[:country_id]).order('name ASC')
-    res = ["<option value=\"\">#{I18n.translate(:please_select)}</option>"]
-    unless states.empty?
-      states.each do |s|
-        res << "<option value=\"#{s.id}\">#{s.name}</option>"
-      end
-    end
-
-    render :json => {:states => res.join}
-
+    readable_states = states.map{ |state| { :id => state.id, :abbr => state.abbr, :name => state.name } }
+    render :json => { :country_id => params[:country_id], :states => readable_states }
   end
 end
